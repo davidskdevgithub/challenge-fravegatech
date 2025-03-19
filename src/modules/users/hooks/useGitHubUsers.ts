227c1eefe0af66d2a72edc2cf59371f774978a64
davidsk <davidsk.dev@gmail.com>
2025-03-19 00:58:38 -0300

    feat: implement search
    - new search-input component
    - new useDebounce hook
    - useGitHubUsers dispatch services depend of query

--- src/modules/users/hooks/useGitHubUsers.ts
+++ src/modules/users/hooks/useGitHubUsers.ts
@@ -3,16 +3,26 @@ import { GitHubUser } from '../github-types';
 import githubService, { githubQueryKeys } from '../services/github-api';
 
 export const useGitHubUsers = <T = GitHubUser[]>(
+  query: string,
   options?: UseQueryOptions<GitHubUser[], Error, T>
 ) => {
+
+  const { queryKey, queryFn } = query.length > 0 ? {
+    queryKey: githubQueryKeys.searchUsers(query),
+    queryFn: () => githubService.searchUsers(query),
+  } : {
+    queryKey: githubQueryKeys.users(),
+    queryFn: () => githubService.getUsers(),
+  };
+
   const {
     data: users = [],
     isLoading,
     error,
     refetch,
   } = useQuery({
-    queryKey: githubQueryKeys.users(),
-    queryFn: () => githubService.getUsers(),
+    queryKey,
+    queryFn,
     ...options,
   });
 
